153a9425feb6543d2caab5d5138602b5c63710d3
Akash <akashikur011@gmail.com>
2023-08-23 22:19:59 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
+import React, { useState } from "react";
 
-import React from "react";
-import './../styles/App.css';
+const Autocomplete = ({ options }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
-const App = () => {
+  const handleInputChange = (event) => {
+    const newInputValue = event.target.value;
+    setInputValue(newInputValue);
+
+    // Filter the options based on the input value
+    const newSuggestions = options.filter((option) =>
+      option.toLowerCase().includes(newInputValue.toLowerCase())
+    );
+
+    setSuggestions(newSuggestions);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Type a fruit..."
+      />
+      <ul>
+        {suggestions.map((suggestion, index) => (
+          <li key={index}>{suggestion}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
+};
+
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Search item</h1>
+      <Autocomplete options={fruits} />
+    </div>
+  );
 }
 
-export default App
+export default App;
